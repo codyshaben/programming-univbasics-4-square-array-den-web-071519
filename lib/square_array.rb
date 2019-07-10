f4410879d3c75156c3123373b1a94b1a4cffc89a
Cody Shaben <cshaben87@gmail.com>
2019-07-10 22:12:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,7 @@ def square_array(numbers)
   while counter < numbers.length do
     puts numbers[counter] = numbers[counter] * numbers[counter]
     counter += 1
-    puts square_array(numbers)
   end
 end
 
+numbers
